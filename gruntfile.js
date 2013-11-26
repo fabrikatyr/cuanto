f2d6bd0b4c442c7715ebedac6dcdf798245d4527
xiaolei <tristones.liu@gmail.com>
2013-11-26 09:41:01 +0800

    setup test directory structure

--- gruntfile.js
+++ gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function(grunt) {
             options: {
                 reporter: 'spec'
             },
-            src: ['test/**/*.js']
+            src: ['test/backend/**/*.js']
         },
         bower: {
             install: {
